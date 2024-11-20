4c08a6ae415bf27227367761a1a3c0cde683dae3
R.Kekua <romski48@gmail.com>
2024-11-20 23:24:23 +0400

    work with file

---
+++ main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("C:\\Users\\Student\\Desktop\\tx.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	var hello string = "hello World"
+	arry := make([]byte, len(hello))
+
+	for i := 0; i < len(hello); i++ {
+		arry[i] = byte(hello[i])
+	}
+
+	_, err = file.Write(arry)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	_ = file.Close()
+}
