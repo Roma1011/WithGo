c8e8c79ada61f1e7ac22db065b70c244715ec44a
R.Kekua <romski48@gmail.com>
2025-01-16 21:12:18 +0400

    modify

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 ﻿package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	file, err := os.OpenFile("C:\\Users\\Student\\Desktop\\tx.txt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		_ = fmt.Errorf("Error opening file:", err)
 		return
 	}
-	var hello string = "hello World"
+	var result int = 0
+	result, err = divide(5, 3)
+	if err != nil {
+		_ = fmt.Errorf("Error opening file:", err)
+		return
+	}
+	var hello string = strconv.Itoa(result)
 	arry := make([]byte, len(hello))
 
 	for i := 0; i < len(hello); i++ {
@@ -24,4 +32,11 @@ func main() {
 		return
 	}
 	_ = file.Close()
+}
+
+func divide(i, j int) (int int, err error) {
+	if i == 0 || j == 0 {
+		err = errors.New("cannot divide by zero")
+	}
+	return i / j, err
 }
